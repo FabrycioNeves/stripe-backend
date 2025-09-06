299f7176829d493cb04e88fe94d2d7bb70f7e817
Fabrycio Nevese <nevesfabrycio@gmail.com>
2025-09-06 20:05:20 -0300

    adiciona endpoint Stripe

---
+++ api/create-payment-intent.js
@@ -0,0 +1,23 @@
+// api/create-payment-intent.js
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      const { amount } = req.body;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd", // ou 'brl'
+      });
+
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+};
